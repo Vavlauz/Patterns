40e52ec2bbd357d4f94e282325f7b7134bafa586
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-03-30 23:23:28 +0700

    initial commit

---
+++ src/test/java/ru/netology/domain/RequestInfo.java
@@ -0,0 +1,28 @@
+package ru.netology.domain;
+
+import lombok.Data;
+
+@Data
+public class RequestInfo {
+    private final String city;
+    private final String name;
+    private final String phone;
+
+    public RequestInfo(String city, String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
