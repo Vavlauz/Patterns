99bcf8b4057da619d9028b4cd9bfb4dd4ded0811
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-03-31 21:17:18 +0700

    initial commit

--- src/test/java/ru/netology/domain/DataGenerator.java
+++ src/test/java/ru/netology/domain/DataGenerator.java
@@ -1,6 +1,7 @@
 package ru.netology.domain;
 
 import com.github.javafaker.Faker;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
--- src/test/java/ru/netology/domain/RequestInfo.java
+++ src/test/java/ru/netology/domain/RequestInfo.java
@@ -10,21 +10,4 @@ public class RequestInfo {
     private final String name;
     private final String phone;
 
-//    public RequestInfo(String city, String name, String phone) {
-//        this.name = name;
-//        this.phone = phone;
-//        this.city = city;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getPhone() {
-//        return phone;
-//    }
-//
-//    public String getCity() {
-//        return city;
-//    }
 }
